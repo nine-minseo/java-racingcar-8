763b9e72a7cbc94d12b2959402644c2c670c0d4f
구민서 <minseo12379@gmail.com>
2026-01-05 22:04:47 +0900

    feat: 자동차 이름 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,4 +1,12 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class Application {
+    public static void main(String[] args) {
+        String carNames = InputView.readCarNames();
+
+        String[] cars = carNames.split(",");
+
+    }
 }
