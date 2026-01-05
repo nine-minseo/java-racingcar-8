07516af94acc8ecf4d8757b715b59cab96c36370
구민서 <minseo12379@gmail.com>
2026-01-05 22:08:36 +0900

    feat: 시도 횟수 입력받는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,8 +5,9 @@ import racingcar.view.InputView;
 public class Application {
     public static void main(String[] args) {
         String carNames = InputView.readCarNames();
-
         String[] cars = carNames.split(",");
+
+        int tryCount = Integer.parseInt(InputView.readTryCount());
 
     }
 }
