4743cd0991299965e241fb178284d0e11aa505a6
구민서 <minseo12379@gmail.com>
2026-01-05 22:11:24 +0900

    feat: 0~9 사이의 무작위 값을 구하는 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.view.InputView;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +9,8 @@ public class Application {
         String[] cars = carNames.split(",");
 
         int tryCount = Integer.parseInt(InputView.readTryCount());
+
+        int roundCount = Randoms.pickNumberInRange(0, 9);
 
     }
 }
